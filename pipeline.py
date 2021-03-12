adca5e8fa2315a85e5132ffcfcc22bb07ed2133e
y-du <42994541+y-du@users.noreply.github.com>
2021-03-12 10:00:57 +0100

    add functions for loading pickle from file or bytes

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 # import pandas as pd
 # import numpy as np
+import pickle
 from types import SimpleNamespace
 # from .data_merge import create_global_dataframe
 from .data_cleansing import impute_missing_values, value_based_column_filter, one_hot_encode_categories, remove_correlated_features, remove_error_codes
@@ -25,6 +26,16 @@ def df_from_csv(csv_path: str, time_col: str, sorted=False):
     df[time_col] = dd.to_datetime(df[time_col])
     df = df.set_index(time_col, sorted=sorted)
     return df
+
+
+def clf_from_pickle_file(pickle_file_path: str):
+    with open(pickle_file_path, 'rb') as file:
+        return pickle.load(file)
+
+
+def clf_from_pickle_bytes(pickle_bytes: bytes):
+    return pickle.loads(pickle_bytes)
+
 
 def run_pipeline(df, config, clf):
     c = SimpleNamespace(**config)
