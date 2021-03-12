155c137063138ecb5a2ec0ad4838f699a022194a
y-du <42994541+y-du@users.noreply.github.com>
2021-03-12 09:53:35 +0100

    use new logger

--- monitoring/time_it.py
+++ monitoring/time_it.py
@@ -14,6 +14,10 @@ import hashlib
 from ..config import debug_mode, write_monitoring, store_results
 from ..config import configs_pipeline
 from ..tools.signature import GLOBAL_SIGNATURE
+from ..logger import get_logger
+
+logger = get_logger(__name__.split(".", 1)[-1])
+
 
 def mem():
     process = psutil.Process(os.getpid())
@@ -28,7 +32,7 @@ def timing(f):
         ts = time()
         time_start = datetime.datetime.fromtimestamp(ts)
         memory_usage_start = mem()
-        print(time_start.strftime('%Y-%m-%d %H:%M:%S'), '[START]', f.__name__)
+        logger.debug('[START] ' + f.__name__)
         skip_store_result_list = ['data_extraction_cpt', 'gen_error_code_indicators', 'remove_error_codes', 'calculate_statistics', #'extract_windows_and_features',
                                   'extract_examples', 'remove_global_timestamp',#'standardize_features',
                                   'get_x_y', 'eval', 'eval_classifier']
@@ -46,7 +50,7 @@ def timing(f):
             df_disp = "columns: " + str(len(result.columns))
         else:
             df_disp = ''
-        print (time_end.strftime('%Y-%m-%d %H:%M:%S'), '[END] %r  %2.0f min' % (f.__name__,  round((te-ts)/60)), memory_disp,  df_mem + ' GB;', df_disp )
+        logger.debug('[END] %r  %2.0f min' % (f.__name__,  round((te-ts)/60)) + memory_disp + df_mem + ' GB; ' + df_disp)
         if write_monitoring:
             df = pd.DataFrame({ 'ts': [ts], 'te': [te], 'ts-te':[te-ts], 'function_name':[f.__name__],'memory_usage_start': memory_usage_start, 'memory_usage_end':  memory_usage_end, 'data_memory': df_mem })
             df.to_csv('../monitoring/time.csv', mode='a', index = False, header=None, sep=";", decimal=",")
@@ -66,23 +70,23 @@ def create_or_load_result(f, *args, **kw):
     else:
         signature_new = GLOBAL_SIGNATURE.sig + f.__name__ + kwargs_repr + str(sorted(glob.glob(args[0])))
     GLOBAL_SIGNATURE.sig = f.__name__ + '_' + hashlib.md5(signature_new.encode('utf-8')).hexdigest()
-    print('Signature after {}: {}'.format(f.__name__, GLOBAL_SIGNATURE.sig))
+    logger.debug('Signature after {}: {}'.format(f.__name__, GLOBAL_SIGNATURE.sig))
     file_path = '../data/Intermediate_Results\\' + GLOBAL_SIGNATURE.sig +str('.parquet.gzip')
     #file_path = '../data/Intermediate_Results\\' + f.__name__ +str('.parquet.gzip')
     available_paths = glob.glob('../data/Intermediate_Results/*.gzip')
     if file_path in available_paths:
         if f.__name__ not in ['extract_windows_and_features', 'standardize_features']:
             result = dd.read_parquet(file_path)
-            print('parquet read')
+            logger.debug('parquet read')
         if f.__name__ in ['extract_windows_and_features', 'standardize_features']:
             result = pd.read_parquet(file_path)
-            print('pandas read')
+            logger.debug('pandas read')
     else:
         result = f(*args, **kw)
         if f.__name__ not in ['extract_windows_and_features', 'standardize_features']:
             result.to_parquet(file_path)
-            print('parquet created')
+            logger.debug('parquet created')
         if f.__name__ in ['extract_windows_and_features', 'standardize_features']:
             result.to_parquet(file_path)
-            print('pandas created')
-    return result   
+            logger.debug('pandas created')
+    return result
