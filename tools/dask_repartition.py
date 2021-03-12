be3008b9bae3914828c69dd22a1b4962def2e3c9
y-du <42994541+y-du@users.noreply.github.com>
2021-03-12 09:53:42 +0100

    use new logger

--- tools/dask_repartition.py
+++ tools/dask_repartition.py
@@ -1,10 +1,12 @@
+from ..logger import get_logger
 
+logger = get_logger(__name__.split(".", 1)[-1])
 
 from ..monitoring.time_it import timing
 
 #@timing
 def dask_repartition(df):
-        print('Partitions ' + str(df.npartitions)+ ' ... repartitioning ...')
+        logger.debug('Partitions ' + str(df.npartitions)+ ' ... repartitioning ...')
         df = df.repartition(partition_size="100MB")
-        print('Partitions: ' + str(df.npartitions))
+        logger.debug('Partitions: ' + str(df.npartitions))
         return df
